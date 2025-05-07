fcb1dfcc7175891f35471ac8985c49361036c3a3
DrumskY <bartekholikti1@gmail.com>
2025-05-07 14:20:35 +0200

    details page change widgets settings

--- lib/pages/pokemon_details_page.dart
+++ lib/pages/pokemon_details_page.dart
@@ -24,140 +24,155 @@ class PokemonDetailsPage extends StatelessWidget {
             : const Color(0xFFA8A77A);
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            top: 50,
-            left: 15,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          SizedBox(
+            height: 380,
+            child: Stack(
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          right: 25.0,
-                        ), // tutaj ustawiasz odstęp z prawej
-                        child: Icon(Icons.arrow_back, color: Colors.white),
+                Positioned(
+                  top: 50,
+                  left: 15,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 25.0),
+                              child: Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Details',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1.5,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    Text(
-                      'Details',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.5,
+                      Text(
+                        pokemon.id < 100
+                            ? pokemon.id < 10
+                                ? "#00${pokemon.id}"
+                                : "#0${pokemon.id}"
+                            : '"#${pokemon.id}"',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 221, 221, 221),
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Text(
-                  pokemon.id < 100
-                      ? pokemon.id < 10
-                          ? "#00${pokemon.id}"
-                          : "#0${pokemon.id}"
-                      : '"#${pokemon.id}"',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 221, 221, 221),
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  pokemonName,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Wrap(
-                  alignment: WrapAlignment.center,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 8,
-                  children:
-                      pokemon.types!.map((type) {
-                        return Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              height: 48,
-                              width: 48,
-                              decoration: BoxDecoration(
-                                color: pokemonTypeColors[type] ?? Colors.grey,
-                                shape: BoxShape.circle,
-                                boxShadow: [
-                                  BoxShadow(
-                                    // ignore: deprecated_member_use
-                                    color: Colors.black.withOpacity(0.5),
-                                    blurRadius: 6,
-                                    offset: Offset(0, 3),
+                      Text(
+                        pokemonName,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Wrap(
+                        alignment: WrapAlignment.center,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        spacing: 8,
+                        children:
+                            pokemon.types!.map((type) {
+                              return Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Container(
+                                    height: 48,
+                                    width: 48,
+                                    decoration: BoxDecoration(
+                                      color:
+                                          pokemonTypeColors[type] ??
+                                          Colors.grey,
+                                      shape: BoxShape.circle,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          // ignore: deprecated_member_use
+                                          color: Colors.black.withOpacity(0.5),
+                                          blurRadius: 6,
+                                          offset: Offset(0, 3),
+                                        ),
+                                      ],
+                                    ),
+                                    padding: EdgeInsets.all(8),
+                                    child: SvgPicture.asset(
+                                      'assets/types/$type.svg',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    type[0].toUpperCase() + type.substring(1),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ],
-                              ),
-                              padding: EdgeInsets.all(8),
-                              child: SvgPicture.asset(
-                                'assets/types/$type.svg',
-                                fit: BoxFit.contain,
-                              ),
-                            ),
-                            SizedBox(height: 4),
-                            Text(
-                              type[0].toUpperCase() +
-                                  type.substring(1), // "fire" -> "Fire"
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        );
-                      }).toList(),
+                              );
+                            }).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  left: -20,
+                  child: Opacity(
+                    opacity: 0.1,
+                    child: Image.asset(
+                      'assets/pokeball.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                ),
+                Positioned.fill(
+                  top: 300,
+                  child: ClipPath(
+                    clipper: SmileClipper(),
+                    child: Container(
+                      color: Colors.white,
+                      padding: EdgeInsets.only(top: 00),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Image.network(
+                      (pokemon.sprites.other?.frontDefault?.isNotEmpty ?? false)
+                          ? pokemon.sprites.other!.frontDefault
+                          : pokemon.sprites.frontDefault,
+                      width: 250,
+                      height: 250,
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
-          Positioned(
-            top: 100,
-            left: -20,
-            child: Opacity(
-              opacity: 0.1,
-              child: Image.asset(
-                'assets/pokeball.png',
-                width: 200,
-                height: 200,
-              ),
-            ),
-          ),
-          Positioned.fill(
-            top: 300,
-            child: ClipPath(
-              clipper: SmileClipper(),
-              child: Container(
-                color: Colors.white,
-                padding: EdgeInsets.only(top: 00),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: Image.network(
-                (pokemon.sprites.other?.frontDefault?.isNotEmpty ?? false)
-                    ? pokemon.sprites.other!.frontDefault
-                    : pokemon.sprites.frontDefault,
-                width: 250,
-                height: 250,
-              ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Center(child: Card(child: Text('Test'))),
             ),
           ),
         ],
